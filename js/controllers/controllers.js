68d4cc9b35633172c0128b49714a78c3d4ac3eaf
Jayesh Kanbi <jkanbi@gmail.com>
2014-10-16 12:35:25 +0100

    Move manuals controller into its own file and update accordingly.

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -56,29 +56,6 @@
 		
 	});
 
-	myBoilerApp.controller('manualsCtrl',['$scope','$firebase',function($scope,$firebase) {
-			var ref =  new Firebase("https://myboiler.firebaseio.com/brands");
-			var sync = $firebase(ref);
-			var syncobject = sync.$asObject();
-			$scope.brands = syncobject;
-			syncobject.$loaded().then(function() {
-    		 console.log("record has id", syncobject.$id);
-  			});
-
-			//ref.on('child_added', function(snapshot) {
-        	//	var message = snapshot.val();
-        	//	$scope.documents = message;
-        		//displayChatMessage (message.name, message.text);
-      		//});  
-			//sync.$loaded().then(function() {
-     		//	console.log("list has " + list.length + " items");
-  			//});
-			
-
-			//$scope.documents = $firebase(ref).$asobject();
-		}
-	]);
-
 	myBoilerApp.controller('enggSearchCtrl',['$scope','saveDataService', function($scope, saveDataService)	
 	{
 		$scope.BoilerFuelTypes = 
