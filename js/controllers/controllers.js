73d0a5b762f9504ba96522f92ccc34c1843fcea9
Jayesh Kanbi <jkanbi@gmail.com>
2014-10-16 12:29:52 +0100

    Move documents controller into seperate file and update links.

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -79,25 +79,6 @@
 		}
 	]);
 
-		myBoilerApp.controller('documentsCtrl',['$scope','$firebase','$routeParams', function($scope,$firebase,$routeParams) {
-			// The following worked to get params , http://www.thinkster.io/angularjs/w9lVd0qRp6/angularjs-routeparams
-			var brandId = $routeParams.brandId;
-			$scope.brandId = brandId;
-			console.log(brandId);
-			
-			var ref =  new Firebase("https://myboiler.firebaseio.com/brands/" + brandId + "/Documents");
-			var sync = $firebase(ref);
-			var syncobject = sync.$asObject();
-			$scope.documents = syncobject;
-			syncobject.$loaded().then(function() {
-    		 console.log("record has id", syncobject.$id);
-  			});
-
-		}
-	]);
-
-	// will display Manual page's content
-
 	myBoilerApp.controller('enggSearchCtrl',['$scope','saveDataService', function($scope, saveDataService)	
 	{
 		$scope.BoilerFuelTypes = 
