fcfb609a9c76ac0968c87c234557a0a02cb9a358
Jayesh Kanbi <jkanbi@gmail.com>
2014-10-24 18:37:57 +0100

    Account feature - not working but testing ftp.

---
+++ js/controllers/account.js
@@ -0,0 +1,10 @@
+/* Controllers */
+
+//Wrapping your javascript in a closure is a good habit! From CodeSchool.
+(function () {
+
+		// create controller name
+		myBoilerApp.controller('accountCtrl',function($scope){
+			console.log("accountCtrl");
+		});
+})();
