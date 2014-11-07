2f4db49967cf34a59cdc263fdfcbe765b407687d
Jayesh Kanbi <jkanbi@gmail.com>
2014-11-07 18:45:34 +0000

    Cookie Script Code cleanup.

--- app/scripts/custom/cookiescript.js
+++ app/scripts/custom/cookiescript.js
@@ -54,18 +54,3 @@ $(document).ready(function () {
 			}
 		}
 });
-
-function email_validate()
-{
-	var email = $('#sub_email').val();
-	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-	if(email != undefined && email != '' && re.test(email))
-		return true;
-	else
-	{
-		alert('Please enter valid email address');
-		$('#sub_email').focus();
-		return false;
-	}
-};
