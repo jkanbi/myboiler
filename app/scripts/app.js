42c0946179357660ccbb8c497d61168d8bb1ad45
Jayesh Kanbi <jkanbi@gmail.com>
2014-11-07 07:27:01 +0000

    Update to Google Analytics for Angular Js...
    ...App code changed to allow SPA pageviewshttp://stackoverflow.com/questions/10713708/tracking-google-analytics-page-views-with-angular-js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -134,6 +134,20 @@
 			$locationProvider.html5Mode(true);
 		  } */
   }]);
+
+	myBoilerApp.run(['$rootScope', '$location', '$window',
+    function($rootScope, $location, $window) {
+      $rootScope.$on('$routeChangeSuccess',
+        function(event) {
+          if (!$window.ga) {
+            return;
+          }
+          $window.ga('send', 'pageview', {
+            page: $location.path()
+          });
+        });
+    }
+  ]);
   
 //})();
 
