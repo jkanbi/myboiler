d471e15fd064a117085d1fff9442f1a3d3318584
Jayesh Kanbi <jkanbi@gmail.com>
2014-12-28 20:43:11 +0000

    Markdown directive for content and blog engine....
    ...Pushed previously for content but some mistake in lines.

--- scripts/directives/directives.js
+++ scripts/directives/directives.js
@@ -1,6 +1,21 @@
-
 /*  Directives  */
-		myBoilerApp.directive('contentMarkdown', function ($http) {
+myBoilerApp.directive('markdown', function ($http) {
+    var converter = new Showdown.converter();
+    return {
+      restrict: 'A',
+      scope: { link:'@' },
+      link: function (scope, element, attrs) {
+        attrs.$observe('link',function(link){
+            $http.get('components/blog/files/' + link).success(function(response){
+              var htmlText = converter.makeHtml(response);
+              element.html(htmlText);
+            });
+        });
+      }
+    };
+  });
+
+myBoilerApp.directive('contentMarkdown', function ($http) {
     var converter = new Showdown.converter();
     return {
       restrict: 'A',
