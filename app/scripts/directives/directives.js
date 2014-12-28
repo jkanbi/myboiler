c752e7ed6b9cd0c22772365e27a20868d819c9ba
Jayesh Kanbi <jkanbi@gmail.com>
2014-12-28 20:39:45 +0000

    Consumer Services converted to Markdown...
    Currently Directive is duplicated from blog component

--- app/scripts/directives/directives.js
+++ app/scripts/directives/directives.js
@@ -1,3 +1,17 @@
 
 /*  Directives  */
-		
+		myBoilerApp.directive('contentMarkdown', function ($http) {
+    var converter = new Showdown.converter();
+    return {
+      restrict: 'A',
+      scope: { link:'@' },
+      link: function (scope, element, attrs) {
+        attrs.$observe('link',function(link){
+            $http.get('components/content/files/' + link).success(function(response){
+              var htmlText = converter.makeHtml(response);
+              element.html(htmlText);
+            });
+        });
+      }
+    };
+  });
