cb5da75cba4b04e7e2e833d572ae128ed270175c
Jayesh Kanbi <jkanbi@gmail.com>
2015-01-01 17:01:35 +0000

    Code, Directory and File restructure: Contact form.

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -18,40 +18,4 @@
   		myPageCtx.footerUrl = erootdir + 'footer.html';
 	});
 
-	myBoilerApp.controller('consumerContactController',function($scope, contactsService)
-	{
-		$scope.EMAIL_REGEXP = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i;
-		
-		$scope.master = {};
-
-		$scope.captchaCode = function()
-		{
-			var min = 1000;
-			var max = 9999;
-			var num = Math.floor(Math.random() * (max - min + 1)) + min;
-			//alert(num);
-			$scope.generatedCaptcha = num;
-		}
-		
-		$scope.noSelection = function()
-		{		
-			alert("Copy Disabled");
-		}
-		
-		$scope.saveContact = function()
-		{
-			var status = contactsService.saveContactInfo($scope.contact);
-			alert("contact submit");
-			$scope.reset();
-
-		}
-
-		$scope.reset =  function ()
-		{
-			$scope.contact = angular.copy($scope.master);
-			$scope.contactsubmit = false;
-			$scope.consumercontact.$setPristine();
-		}
-	});
-
 })();
