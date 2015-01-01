0191d74b30c833526818f1e560a9a7c2a8065dcb
Jayesh Kanbi <jkanbi@gmail.com>
2015-01-01 15:58:58 +0000

    Code Refactor: Move Controllers BlogCtrl to blogApp.js

--- scripts/controllers/controllers.js
+++ scripts/controllers/controllers.js
@@ -18,20 +18,6 @@
   		myPageCtx.footerUrl = erootdir + 'footer.html';
 	});
 
-	myBoilerApp.controller('blogCtrl',['$scope', function($scope)
-	{
-		var myDataRef = new Firebase('https://myboiler.firebaseio.com/blogdata/categories').on('child_added',show);
-		
-		function show(snap) 
-		{
-			var item = snap.val();
-			var category = snap.val();  //use name to get child name
-			var itemDashed = item.toString().replace(/\s+/g, '-').toLowerCase();
-			$('.showCategory').append("<li class='cat-item'><a href='http://www.myboiler.com/blog/"+ itemDashed +"'>"+ category +"</a></li>");
-			//console.log(snap);
-		}
-	}]);
-
 	myBoilerApp.controller('consumerContactController',function($scope, contactsService)
 	{
 		$scope.EMAIL_REGEXP = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i;
