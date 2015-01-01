9fde3ae7ff422d2a72ff9515e539a3e1dbf2487d
Jayesh Kanbi <jkanbi@gmail.com>
2015-01-01 12:53:07 +0000

    Directory Structure: Authentication and User Services renamed and moved to components/userlogin

--- components/userlogin/auth-service.js
+++ components/userlogin/auth-service.js
@@ -4,7 +4,7 @@
 myBoilerApp.factory('Auth',
   function ($firebaseAuth, FIREBASE_URL, $rootScope) {
     var ref = new Firebase(FIREBASE_URL);
-
+    $rootScope.authObj = $firebaseAuth(ref);
     var Auth = {
       register: function (user) {
         return ref.createUser({"email" : user.email, "password": user.password}, function(error) {
--- components/userlogin/auth.js
+++ components/userlogin/auth.js
@@ -3,21 +3,17 @@
 myBoilerApp.controller('AuthCtrl',
   function ($scope, $location, $firebaseAuth, FIREBASE_URL, User) {
     var ref = new Firebase(FIREBASE_URL);
-    
+    var user = User;
+
     $scope.authObj = $firebaseAuth(ref);
     var authData = $scope.authObj.$getAuth();
     
+    $scope.authObj.$onAuth(function () {
     if (authData) {
       $location.path('/');
     }
-
-    /*$scope.authObj.$onAuth(function () {
-      console.log("started");
-      $location.path('/');
-      console.log("finished");
     });
-    */
-
+    
     $scope.login = function () {
       $scope.authObj.$authWithPassword({
         email:$scope.user.email,
@@ -38,7 +34,19 @@ myBoilerApp.controller('AuthCtrl',
           password: $scope.user.password
         });
       }).then(function(authData) {
-        User.create(authData,$scope.user.username,$scope.user.email);
+        //user.create(authData);
+        //Add the user to firebase 'user' data
+        // we would probably save a profile when we register new users on our site
+        // we could also read the profile to see if it's null
+        // here we will just simulate this with an isNewUser boolean
+        var isNewUser = (ref.child("users").child(authData.uid) == null)?false:true;
+        ref.onAuth(function(authData) {
+          if (authData && isNewUser) {
+            // save the user's profile into Firebase so we can list users,
+            // use them in Security and Firebase Rules, and show profiles
+            ref.child("users").child(authData.uid).set(authData);
+          }
+        });
         console.log("Logged in as:", authData.uid);
         $location.path('/');
       }).catch(function(error) {
